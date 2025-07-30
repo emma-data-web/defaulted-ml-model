83bdd7cb23a2263da46bafef1cc35b93beb0b325
emma-data-web <emmanuelnwankwo690@gmail.com>
2025-07-30 18:06:18 +0100

    still building

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,24 @@
 from feature_engineering import add_feature
+from db_pull import get_data
 import pandas as pd
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
 from xgboost import XGBClassifier
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.compose import f
+
+
+df = get_data()
+
+
+features = ['Employed', 'Bank Balance' ,'Annual Salary', 'Defaulted?',              'high_earners', 'low_balance']
+
+transformer_features = FunctionTransformer(add_feature, validate=False)
+
+processing = Pipeline(steps=[
+  ('impute', SimpleImputer(strategy='mean'))
+])
+
+column_transformer = ColumnTransformer(tra)
